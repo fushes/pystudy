22e83dedbcde5146a78efee63d958f877781699a
fushes <67745567+fushes@users.noreply.github.com>
2023-08-29 01:13:59 +0800

    测试

--- DelFile.py
+++ DelFile.py
@@ -1,16 +1,9 @@
-from graph_onedrive import OneDriveManager
+import sys
+import asyncio
+sys.path.append("..")
+from nfysd import sdn
 
-# Use a context manager to manage the session
-with OneDriveManager(config_path="config.json", config_key="onedrive") as my_drive:
+async def test():
+    await sdn("签到错误","获取登录状态失败！","https://www.baidu.com")
 
-    my_drive.get_usage(verbose=True)
-
-    items = my_drive.list_directory(folder_id="01CVXDX5BOYX7TAEL6PBFIRA7D37DECXLP", verbose=False)
-
-    if len(items) > 7:
-        for i in range(0,7):
-            print("删除文件："+items[i]['name'])
-            confirmation = my_drive.delete_item(items[i]['id'], pre_confirm=True)
-            print(confirmation)
-    else:
-        print("当前文件数量未达到设定值")
+asyncio.run(test())
