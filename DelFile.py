eb681176db6d5f258b738481ca393cf894f87a1a
noRice <67745567+fushes@users.noreply.github.com>
2025-05-21 14:05:08 +0800

    更新版本

--- DelFile.py
+++ DelFile.py
@@ -1,9 +1,11 @@
-import sys
-import asyncio
-sys.path.append("..")
-from nfysd import sdn
-
-async def test():
-    await sdn("签到错误","获取登录状态失败！","https://www.baidu.com")
-
-asyncio.run(test())
+import requests, sys
+url = 'https://2022.ipchaxun.com/'
+proxy = {
+    'http' : 'http://120.42.46.226:6666',
+    'https' : 'http://120.42.46.226:6666'
+}
+try:
+    response = requests.get(url, proxies=proxy, timeout=30)
+    print(response.content.decode('utf-8'))
+except requests.exceptions.ConnectionError:
+    print('超时')
